dfd9c49edffeb118e6bd43840d00e4782ad5916b
whhub <wanghui122004@me.com>
2017-04-20 17:59:02 +0800

    [ViewerToolBox][Version013][ViewerToolInterface] IViewerTool

---
+++ ViewerToolInterface/IViewerTool.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ViewerToolInterface
+{
+    public interface IViewerTool
+    {
+        string Name { get; }
+        void Act();
+        event EventHandler CanActChanged;
+        bool CanAct { get; }
+    }
+}
