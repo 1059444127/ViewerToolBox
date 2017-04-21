25169ddbaf6afeecc0ca2dccca300bc2f7dcac16
whhub <wanghui122004@me.com>
2017-04-21 15:08:01 +0800

    [ViewerToolBox][Version014] [ViewerToolBox] Add UserControl ViewerToolBox

---
+++ ViewerToolBox/ViewerToolBox.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ViewerToolBox
+{
+    /// <summary>
+    /// Interaction logic for ViewerToolBox.xaml
+    /// </summary>
+    public partial class ViewerToolBox
+    {
+        public ViewerToolBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
