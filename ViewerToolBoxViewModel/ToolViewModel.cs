4b395c3ab3ddc3df8a87511ea7427b10a12c59dc
whhub <wanghui122004@me.com>
2017-04-25 16:24:11 +0800

    [ViewerToolBox][Version015] [ViewerToolBoxViewModel] Add Class ToolViewModel  which depends on an IViewerTool

---
+++ ViewerToolBoxViewModel/ToolViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using ViewerToolInterface;
+
+namespace ViewerToolBoxViewModel
+{
+    public class ToolViewModel : ViewModelBase
+    {
+        public ToolViewModel(IViewerTool tool)
+        {
+            _tool = tool;
+            _tool.CanActChanged += ToolOnCanActChanged;
+        }
+
+        ~ToolViewModel()
+        {
+            _tool.CanActChanged -= ToolOnCanActChanged;
+        }
+
+        private void ToolOnCanActChanged(object sender, EventArgs eventArgs)
+        {
+            _toolCommand.RaiseCanExecuteChanged();
+        }
+
+        private readonly IViewerTool _tool;
+
+        public string Name { get { return _tool.Name; } }
+
+        #region [--ToolCommand--]
+
+        private RelayCommand _toolCommand;
+
+        public ICommand ToolCommand
+        {
+            get { return _toolCommand = _toolCommand ?? new RelayCommand(OnToolPicked, CanToolExecute); }
+        }
+
+        private bool CanToolExecute()
+        {
+            return _tool.CanAct;
+        }
+
+        private void OnToolPicked()
+        {
+            _tool.Act();;
+        }
+
+        #endregion [--ToolCommand--]    
+
+    }
+}
