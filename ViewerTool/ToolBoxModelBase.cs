0b0591faf657dc43294b841a6552a2d4c89bca28
whhub <wanghui122004@me.com>
2017-04-28 17:29:33 +0800

    [ViewerToolBox][Version025] [ViewerTool] Add abstract class ToolBoxModelBase : IToolBoxModel,     Export IList<IViewDisplay>

---
+++ ViewerTool/ToolBoxModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using ViewerToolInterface;
+
+namespace ViewerTool
+{
+    public abstract class ToolBoxModelBase : IToolBoxModel
+    {
+        [Export]
+        private readonly IList<IViewDisplay> _viewDisplays = new List<IViewDisplay>();
+
+        #region Implementation of IToolBoxModel
+
+        public void Register(IViewDisplay viewDisplay)
+        {
+            _viewDisplays.Add(viewDisplay);
+        }
+
+        #endregion
+    }
+}
