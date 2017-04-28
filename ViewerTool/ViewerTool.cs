b4f6344389332222bb041c9edd5008e6c8c7c779
whhub <wanghui122004@me.com>
2017-04-28 17:22:34 +0800

    [ViewerToolBox][Version024] [ViewerTool] Add abstract class ViewerTool Realized Interface  IViewerTool

---
+++ ViewerTool/ViewerTool.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using ViewerToolInterface;
+
+namespace ViewerTool
+{
+    public abstract class ViewerTool : IViewerTool
+    {
+        #region Implementation of IViewerTool
+
+        public abstract string Name { get; }
+
+        public void Act()
+        {
+            foreach (var viewDisplay in ViewDisplays)
+            {
+                ActOn(viewDisplay);
+            }
+        }
+        public event EventHandler CanActChanged = delegate {};
+        public abstract bool CanAct { get; }
+
+        #endregion
+
+        #region [--Protected Template--]
+
+        [Import]
+        protected IList<IViewDisplay> ViewDisplays;
+
+        protected void RaiseCanActChangedEvent()
+        {
+            CanActChanged(this, new EventArgs());
+        }
+
+        protected abstract void ActOn(IViewDisplay viewDisplay);
+
+        #endregion
+    }
+}
